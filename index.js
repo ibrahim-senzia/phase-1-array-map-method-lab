fdad43eb06a260ff5d13e594290892808492d64f
ibrahim senzia <ibrahimsenzia@gmail.com>
2024-03-26 21:37:06 +0300

    map-method-lab

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((sentence)=>{
+    //we are splitting each sentence using the space into  words
+    const words=sentence.split(" ")
+    console.log(words)
+    const titlecaseWord= words.map((word)=>{
+        console.log(word)
+       return word.charAt(0).toUpperCase()+ word.slice(1)
+    })
+    const result=titlecaseWord.join(" ")
+    
+    return result
+ })
+  
 }
